f4e0952cab55a2c9127061f62208b553a7e6b1b5
Ethan Belcher <pandurrerock@gmail.com>
2021-06-22 01:40:11 +0000

    1.0.2

--- core.js
+++ core.js
@@ -1,11 +1,11 @@
-window.onload = function {
-    const doge = document.getElementById("#doge");
-    const dogecoin = document.getElementById("#dogecoin");
+const doge = document.getElementById("#doge");
+const dogecoin = document.getElementById("#dogecoin");
 
-    var dc = 0;
+var dc = 0;
 
+if (doge) {
     doge.addEventListener("click", function() {
         dc += 1;
         dogecoin.innerHTML = dc + "Ð"
     });
-};
+}
